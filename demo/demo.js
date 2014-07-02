cb345ba8039ea041116fb915df9d9cb2a28e6a5e
gdhuang <gdhuang@ntu.edu.tw>
2014-07-02 23:42:21 +0800

    fix ast parsing

--- demo/demo.js
+++ demo/demo.js
@@ -1,28 +1,113 @@
 'use strict';
 
-/* allen */
-function simpleInOut(a ,b) {
-        var res = a + 1;
-        return res;
-};
+var fs = require('fs'),
+    path = require('path');
 
 /* allen */
-var funexp = function(a, b) {
-    var res = b + '1';
-    return res;
+function type(a) {
+    if(typeof a === 'string')
+        return 'str('+a+')';
+    else if(typeof a === 'number') 
+        return a+1;
+    else if(typeof a === 'boolean')
+        return !a;
+    else if(typeof a === 'function')
+        return a;
+    else if(typeof a === 'object') {
+        if(a instanceof Array)
+            return a.push(0);
+        else if(a instanceof Object) {
+            a.t = 't';
+            return a;
+        }
+        else
+            return a;
+    }
+    else if(typeof a === 'undefined')
+        return a;
+    else
+        throw 'error';
 };
+
+
+/* allen */
+function params(a, b) {
+    return {'a':type(a), 'b': type(b)};
+};
+
 
 /* allen callback:cb */
-function funAsync(a, cb) {
-    var result = {data: a+1};
+function callback(a, cb) {
+    var result = type(a);
     var msg = "success";
 
     cb(msg, result);
 };
 
 
+var cxtIn;
+var cxtOut;
+/* allen */
+function context() {
+    cxtOut = type(cxtIn);
+}
+
+
+/* allen */
+function contextImplicit() {
+    context();
+}
+
+
+/* allen */
+function stub() {
+
+    var str = fs.readFileSync(__dirname +'/data.json');
+
+    return str;
+}
+
+/* allen */
+function stubTwice() {
+
+    var p = path.join('dir/', 'subdir/'),
+        result;
+
+    result = path.join(p, 'flename');
+
+    return result;
+}
+
+
+/* allen */
+function stubAsync(cb) {
+    fs.readFile(__dirname+'/data.json', function(err, data) {
+        cb(JSON.parse(data));
+    });
+}
+
+/* allen */
+function closure(a) {
+    var f = function() {
+        return type(a);
+    };
+
+    cxtOut = f();
+
+}
+
+
 module.exports = {
-    simpleInOut: simpleInOut,
-    funexp: funexp,
-    funAsync: funAsync
+    type: type,
+    params: params,
+    callback: callback,
+
+    ctxIn: cxtIn,
+    cxtOut: cxtOut,
+    context: context,
+    contextImplicit: contextImplicit,
+
+    stub: stub,
+    stubTwice: stubTwice,
+    stubAsync: stubAsync
 };
--- demo/driver.js
+++ demo/driver.js
@@ -1,10 +1,29 @@
 'use strict';
 var demo = require('./demo.js');
 
-demo.simpleInOut(1, null);
-demo.funexp(null, '1');
 
-demo.funAsync(1, function cb(msg, data) {
-    console.log(msg);
-    console.log(data.data);
-});
+demo.type(1);
+demo.type('1');
+demo.type(true);
+demo.type(function() { console.log('demo.type')});
+demo.type([1]);
+demo.type({'o': 'o'});
+demo.type();
+demo.type(1, '2');
+
+demo.params(1, 2);
+
+demo.callback({data: 'data'}, function(msg, result) { });
+
+
+demo.ctxIn = 'a';
+demo.ctxOut = '';
+demo.context();
+
+demo.contextImplicit();
+
+demo.stub();
+
+demo.stubTwice();
+
+demo.stubAsync(function(obj) { });
